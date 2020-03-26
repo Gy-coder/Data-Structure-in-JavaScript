55434081aaabe250220a01ac4de0bf39f6da610f
Gy <406631892@qq.com>
2020-03-26 16:10:15 +0800

    链表

--- 排序算法/快速排序.js
+++ 排序算法/快速排序.js
@@ -15,13 +15,13 @@ let quickSort = (arr,begin,end) =>{
                 j--
             }
             if(arr[j]<key){
-                swap(arr,i,j)
+                arr[i] = arr[j]
             }
             while(i<j&&arr[i]<key){
                 i++
             }
             if(arr[i]>key){
-                swap(arr,i,j)
+                arr[j] = arr[i]
             }
         }
         arr[i] = key
@@ -34,3 +34,8 @@ let quickSort = (arr,begin,end) =>{
 
 let arr = [9,7,10,2,4,5,6]
 console.log(quickSort(arr,0,6))
+let arr2 = [8,10,9,7,11,11,22,22,14,99,100,1001,1,-1,-10,-9,-90,-1002]
+console.log(quickSort(arr2,0,arr2.length-1))
+let arr3 = [10,0,-2,-9,11,19,25,-3,8,80,9,99]
+console.log(quickSort(arr3,0,arr3.length-1))
+
